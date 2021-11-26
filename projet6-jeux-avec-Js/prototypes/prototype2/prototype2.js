c9de4bf2d63c09980d445d311f3783b59eaeaed0
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-11-26 07:27:34 -0800

    added

---
+++ projet6-jeux-avec-Js/prototypes/prototype2/prototype2.js
@@ -0,0 +1,18 @@
+var guess = document.getElementById("submitGuess")
+var output = document.getElementById("output")
+var message;
+var number = Math.floor(Math.random() * 100)
+ function submit() {
+
+    
+    var input = document.getElementById('guess').value;
+    if (input == number){
+        message = "Bravo est vrai il est " + number;
+    } else if (input > number ) {
+        message= "Essayer un petit nombre !"
+    } else if (input < number ) {
+        message= "Essayer un grand nombre !"
+    }
+    output.innerHTML = message
+}
+

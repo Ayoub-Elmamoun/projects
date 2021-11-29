3f1989c424dec63872d57f9f7eefe799d0cffdd8
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-11-29 03:41:59 -0800

    added

--- projet6-jeux-avec-Js/realisation/jeux.js
+++ projet6-jeux-avec-Js/realisation/jeux.js
@@ -1,1 +1,23 @@
-var = "ali klay"
+var guess = document.getElementById("submitGuess")
+var output = document.getElementById("output")
+var message;
+var number = Math.floor(Math.random() * 100)
+ function submit() {
+
+    
+    var input = document.getElementById('guess').value;
+
+    if (input == number){
+
+        message = "Bravo !! il est " + number;
+
+    } else if (input > number ) {
+
+        message= "Essayer un petit nombre !"
+
+    } else if (input < number ) {
+        
+        message= "Essayer un grand nombre !"
+    }
+    output.innerHTML = message
+}
